429b512f8e3dcc8fd60ea6778ea11cec45886405
yumimac <yumi.ko.cheers@gmail.com>
2016-06-13 02:16:53 +1000

    added routers and more code for views

--- YumiKo/week_07/todo/js/views/todos.js
+++ YumiKo/week_07/todo/js/views/todos.js
@@ -1,14 +1,16 @@
 var app = app || {};
 
 // The DOM element for a todo item...
-app.TodoVeiw = Backbone.View.extend({
+app.TodoView = Backbone.View.extend({
 
   tagName: 'li',
 
   template: _.template( $('#item-template').html() ),
 
   events: {
+    'click .toggle': 'toggleCompleted',
     'dblclick label': 'edit',
+    'click .destroy': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blur .edit': 'close'
   },
@@ -18,13 +20,39 @@ app.TodoVeiw = Backbone.View.extend({
   // app, we set a direct reference on the model for convenience.
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   // Re-renders the titles of the todo item.
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
+
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
     return this;
+  },
+
+  // NEW - Toggles visibility of item
+  toggleVisible: function() {
+    this.$el.toggleClass('hidden', this.isHidden());
+
+  },
+
+  // NEW - Determines if item should be hidden
+  isHidden : function () {
+    var isCompleted = this.model.get('completed');
+    return( //hidden cases only
+      (!isCompleted && app.TodoFilter === 'completed')
+      || (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+
+  // NEW - Toggle the '"completed"' state of the model.
+  toggleCompleted: function() {
+    this.model.toggle();
   },
 
   // Switch this view into '"editing"' mode, displaying the input field.
@@ -49,5 +77,10 @@ app.TodoVeiw = Backbone.View.extend({
     if ( e.which === ENTER_KEY) {
       this.close();
     }
+  },
+
+  // NEW - Remove the item, destroy the model from *localStorage* and delete its view.
+  clear: function() {
+    this.model.destroy();
   }
 });
