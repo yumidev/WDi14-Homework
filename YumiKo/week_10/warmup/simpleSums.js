b2ea615a847ece79e3fced75294dde515613d736
yumimac <yumi.ko.cheers@gmail.com>
2016-06-28 10:25:10 +1000

    added answer for warmup and heard interesting stories about infinity

--- YumiKo/week_10/warmup/simpleSums.js
+++ YumiKo/week_10/warmup/simpleSums.js
@@ -1,17 +1,20 @@
 var letSum = {
   s1: function (n) {
-      if (n%2===0){
-        return 0 || 1;
-      }
-      else {
-        return 1;
-      }
+      // if (n%2===0){
+      //   return 0;
+      // }
+      // else {
+      //   return 1;
+      // }
+      return n%2===0 ? 0 : 1
+      // grandi's series , string theory
   },
   s2: function(n) {
-    var sum = 0;
-    for(var j=1; j<=n; j++){
-      sum += j
-    }
-    return sum;
+    // var sum = 0;
+    // for(var j=1; j<=n; j++){
+    //   sum += j
+    // }
+    // return sum;
+    return (n+1)*n/2;
   }
 }
